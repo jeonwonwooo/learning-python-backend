0604f736a805163c2b25901d6abb9865a89436ba
jeonwonwooo <syarivatunnisai@gmail.com>
2025-12-14 10:43:07 +0700

    phase1-week1-day2: variables and data types

---
+++ phase_1_python/exercises/week_1_basic.py
@@ -0,0 +1,9 @@
+print("hi this is me, semoga saya konsisten haha")
+#batas day 1
+
+name="Reva"
+age=17
+is_student=True
+print("Nama: ", name)
+print("Umur: ", age)
+print("apakah saya pelajar? ", is_student)
