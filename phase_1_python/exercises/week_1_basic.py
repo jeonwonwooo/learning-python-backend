da6a40b24b70cad956a509d26d74a8bd1b55e579
jeonwonwooo <syarivatunnisai@gmail.com>
2025-12-14 10:46:14 +0700

    phase1-week1-day3: input and output

--- phase_1_python/exercises/week_1_basic.py
+++ phase_1_python/exercises/week_1_basic.py
@@ -7,3 +7,9 @@ is_student=True
 print("Nama: ", name)
 print("Umur: ", age)
 print("apakah saya pelajar? ", is_student)
+#batas day 2
+
+user_name=input("Masukkan nama kamu: ")
+user_age=input("Masukkan umur kamu: ")
+print("Halo", user_name)
+print("Umur kamu", user_age)
